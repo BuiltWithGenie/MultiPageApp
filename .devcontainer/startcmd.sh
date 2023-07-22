b2985638cbeebd5e309f62fd3e57fdbdd535eeff
PGimenez <gimenezfebrer@gmail.com>
2023-07-22 23:41:59 +0200

    Delete startcmd.sh

--- .devcontainer/startcmd.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.julia/geniebuilder
-ln -s app/EDA/ ~/.julia/geniebuilder/EDA
-ln -s app/ML/ ~/.julia/geniebuilder/ML
-ln -s app/API/ ~/.julia/geniebuilder/API
-ln -s . ~/.julia/geniebuilder/MultiPage
-julia --startup-file=no --depwarn=no /home/vscode/.vscode-remote/extensions/geniebuilder.geniebuilder-0.4.10/scripts/rungb.jl
