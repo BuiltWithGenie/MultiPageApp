75af6848c213e621efb22e7c2810fbc8dd365522
PGimenez <gimenezfebrer@gmail.com>
2023-07-23 00:59:21 +0200

    Update startcmd.sh

--- .devcontainer/startcmd.sh
+++ .devcontainer/startcmd.sh
@@ -4,6 +4,6 @@ ln -s /workspaces/MultiPageApp/app/ML/ ~/.julia/geniebuilder/ML
 ln -s /workspaces/MultiPageApp/app/API/ ~/.julia/geniebuilder/API
 ln -s /workspaces/MultiPageApp ~/.julia/geniebuilder/MultiPage
 cd /workspaces/MultiPageApp && julia -e "using Pkg; Pkg.instantiate()"
-cd /workspaces/MultiPageApp/app/EDA && julia -e "using Pkg; Pkg.activate(); Pkg.instantiate()"
-cd /workspaces/MultiPageApp/app/ML && julia -e "using Pkg; Pkg.activate(); Pkg.instantiate()"
-cd /workspaces/MultiPageApp/app/API && julia -e "using Pkg; Pkg.activate(); Pkg.instantiate()"
+cd /workspaces/MultiPageApp/app/EDA && julia -e "using Pkg; Pkg.activate("."); Pkg.instantiate()"
+cd /workspaces/MultiPageApp/app/ML && julia -e "using Pkg; Pkg.activate("."); Pkg.instantiate()"
+cd /workspaces/MultiPageApp/app/API && julia -e "using Pkg; Pkg.activate("."); Pkg.instantiate()"
