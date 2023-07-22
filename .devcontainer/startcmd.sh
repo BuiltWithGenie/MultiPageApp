e432db0a50af49e48bdf1ead4454a5ba53a4c598
PGimenez <gimenezfebrer@gmail.com>
2023-07-23 00:52:48 +0200

    Create startcmd.sh

---
+++ .devcontainer/startcmd.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.julia/geniebuilder
+ln -s app/EDA/ ~/.julia/geniebuilder/EDA
+ln -s app/ML/ ~/.julia/geniebuilder/ML
+ln -s app/API/ ~/.julia/geniebuilder/API
+ln -s . ~/.julia/geniebuilder/MultiPage
