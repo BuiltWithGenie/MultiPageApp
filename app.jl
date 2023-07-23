fdada19def5453074720827d1f8b6d82b7a62a59
Pere Giménez <gimenezfebrer@gmail.com>
2023-07-23 23:26:20 +0200

    main template

--- app.jl
+++ app.jl
@@ -1,17 +1,5 @@
 module App
 using GenieFramework
 @genietools
-include("app/EDA/app.jl")
-include("app/ML/app.jl")
-include("app/API/app.jl")
-import .EDA
-import .ML
-import .API
 
-@page("/eda", "app/EDA/app.jl.html", "layout.jl.html", Main.App.EDA)
-@page("/ml", "app/ML/app.jl.html", "layout.jl.html", Main.App.ML)
-@page("/api", Main.App.API.ui, nothing)
-route("/") do
-    redirect(:get_eda)
-end
 end
