2dc7748f3fb82302faa3f1bd5082a541c77103d0
Pere Giménez <gimenezfebrer@gmail.com>
2023-07-24 13:06:29 -0400

    cleanup

---
+++ pkginstall.sh
@@ -0,0 +1,3 @@
+cd app/EDA && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
+cd app/ML && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
+cd app/API && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
