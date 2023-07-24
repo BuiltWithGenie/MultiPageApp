2fe72b3190f2ffd1409387f39d2cbd64f3282697
Pere Giménez <gimenezfebrer@gmail.com>
2023-07-24 13:32:23 -0400

    setup scripts

--- gbsetup.sh
+++ gbsetup.sh
@@ -1,8 +1,6 @@
+#!/bin/bash
 mkdir -p ~/.julia/geniebuilder/apps
 ln -s $(pwd)/app/EDA ~/.julia/geniebuilder/apps/EDA
 ln -s $(pwd)/app/ML ~/.julia/geniebuilder/apps/ML
 ln -s $(pwd)/app/API ~/.julia/geniebuilder/apps/API
 ln -s $(pwd) ~/.julia/geniebuilder/apps/MultiPage
-cd ~/.julia/geniebuilder/apps/EDA && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
-cd ~/.julia/geniebuilder/apps/ML && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
-cd ~/.julia/geniebuilder/apps/API && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
--- pkginstall.sh
+++ pkginstall.sh
@@ -1,3 +1,5 @@
-cd app/EDA && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
-cd app/ML && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
-cd app/API && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
+#!/bin/bash
+CURRENT_DIR=$(pwd)
+cd $CURRENT_DIR/app/EDA && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
+cd $CURRENT_DIR/app/ML && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
+cd $CURRENT_DIR/app/API && julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
