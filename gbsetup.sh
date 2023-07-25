17af534434056dc8aaba692f369b318dcbe77470
Pere Giménez <gimenezfebrer@gmail.com>
2023-07-24 22:18:44 -0400

    gb setup

--- gbsetup.sh
+++ gbsetup.sh
@@ -2,5 +2,3 @@
 mkdir -p ~/.julia/geniebuilder/apps
 ln -s $(pwd)/app/EDA ~/.julia/geniebuilder/apps/EDA
 ln -s $(pwd)/app/ML ~/.julia/geniebuilder/apps/ML
-ln -s $(pwd)/app/API ~/.julia/geniebuilder/apps/API
-ln -s $(pwd) ~/.julia/geniebuilder/apps/MultiPage
